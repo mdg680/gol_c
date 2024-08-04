bbab62066f0d82ff23dd5bcbb496b16fa9e6624a
mdg680 <monoper@gmai.com>
2024-08-04 19:07:24 +0200

    Changed array allocation to use calloc instead of malloc
    bonus:
      - Some formatting improvements

--- grid.c
+++ grid.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "grid.h"
 
 struct grid create_grid(int dim_x, int dim_y)
@@ -8,7 +7,7 @@ struct grid create_grid(int dim_x, int dim_y)
     struct grid grid;
     grid.dim_x = dim_x;
     grid.dim_y = dim_y;
-    grid.grid_array = (int*)malloc(dim_x * dim_y * sizeof(int));
+    grid.grid_array = (int*)calloc(dim_x * dim_y, sizeof(int));
 
     return grid;
 }
@@ -20,7 +19,7 @@ void populate_grid_random(struct grid* grid)
 
 void insert_cell(struct grid* grid, int value, int x, int y)
 {
-    grid->grid_array[y*grid->dim_y + x] = value;
+    grid->grid_array[y * grid->dim_y + x] = value;
 }
 
 void print_grid(struct grid* grid)
@@ -28,7 +27,7 @@ void print_grid(struct grid* grid)
     printf("Print Grid(%d,%d)\n\n", grid->dim_x, grid->dim_y);
     for (int y = 0; y < grid->dim_y; y++){
         for (int x = 0; x < grid->dim_x; x++){
-            printf("%d ", grid->grid_array[x + y*grid->dim_y]);
+            printf("%d ", grid->grid_array[x + y * grid->dim_y]);
         }
         printf("\n");
     }
