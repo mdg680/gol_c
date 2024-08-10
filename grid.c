61d6066e3cfe4ac1aa6c70e00c6d6473b1c87971
mdg680 <monoper@gmai.com>
2024-08-10 21:10:01 +0200

    grid.c changes:
    - Removed unimplemented random fill version: no need to keep until any
      need
    - get_cell method added to simplify syntax related to grid array access
    - Using cell.h typedef for datatype of cell value datatype, this is
      temporary until the cell struct datatype is properly fleshed out and
      implemented.

--- grid.c
+++ grid.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "grid.h"
+#include "cell.h"
 
 grid create_grid(int dim_x, int dim_y)
 {
     grid grid;
     grid.dim_x = dim_x;
     grid.dim_y = dim_y;
-    grid.grid_array = (int*)calloc(dim_x * dim_y, sizeof(int));
+    grid.grid_array = (int*)calloc(dim_x * dim_y, sizeof(cell_data));
 
     return grid;
-}
-
-void populate_grid_random(grid* grid)
-{
-    // TODO
 }
 
 void insert_cell(grid* grid, int value, int x, int y)
@@ -22,12 +18,21 @@ void insert_cell(grid* grid, int value, int x, int y)
     grid->grid_array[y * grid->dim_y + x] = value;
 }
 
+cell_data get_cell(grid* grid, int x, int y)
+{
+    return grid->grid_array[x + y * grid->dim_y];
+}
+
 void print_grid(grid* grid)
 {
     printf("Print Grid(%d,%d)\n\n", grid->dim_x, grid->dim_y);
-    for (int y = 0; y < grid->dim_y; y++){
-        for (int x = 0; x < grid->dim_x; x++){
-            printf("%d ", grid->grid_array[x + y * grid->dim_y]);
+    
+    int dim_y = grid->dim_y;
+    int dim_x = grid->dim_x;
+    
+    for (int y = 0; y < dim_y; y++){
+        for (int x = 0; x < dim_x; x++){
+            printf("%d ", get_cell(grid, x, y));
         }
         printf("\n");
     }
